a1a209c3a8fc23e380883d26067964864418c6a8
Sidharth Verma <94635538+Sidharth465@users.noreply.github.com>
2023-09-12 09:04:55 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import StackNavigator from "./navigation/StackNavigator";
+import { Provider } from "react-redux";
+import store from "./store";
+
+export default function App() {
+  return (
+    <>
+      <Provider store={store}>
+        <StackNavigator />
+        
+      </Provider>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
